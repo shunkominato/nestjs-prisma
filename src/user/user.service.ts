693389f8255c5ef7a8b6b4d34f88cc1459e8319a
shunkominato <shun19871001@gmail.com>
2022-08-19 12:54:49 +0900

    a

---
+++ src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async updateUser(
+    userId: number,
+    dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    console.log(userId);
+    console.log(dto);
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+    delete user.hashedPassword;
+    return user;
+  }
+}
