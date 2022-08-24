b9587d5040a58f269f873597585946833070a9ac
shunkominato <shun19871001@gmail.com>
2022-08-24 23:32:06 +0900

    d

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors({
     credentials: true,
     origin: [
-      'http://localhost:3000',
+      'http://127.0.0.1:3000',
       'https://frontend-todo-nextjs.vercel.app',
     ],
   });
